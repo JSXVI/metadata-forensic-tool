86d1f05fb254cf1b7191a4b4a404c6337aaa18a0
JSXVI <131764108+JSXVI@users.noreply.github.com>
2026-03-14 21:25:04 +0530

    Create root build.gradle.kts with Android plugins configuration

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("com.android.application") version "7.1.2" apply false
+    id("com.android.library") version "7.1.2" apply false
+    id("kotlin-android") version "1.5.31"
+}
+
+// Android application and library configurations can be placed here.
